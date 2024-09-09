7082b6653acaded42eb9185d7187d2ad91063785
alfarofernando <alfarofernando@live.com>
2024-09-09 03:28:04 -0300

    Using multiple JSX Slots

--- react_essentials/src/components/Examples.jsx
+++ react_essentials/src/components/Examples.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import TabButton from "./TabButton";
 import { EXAMPLES } from "./Data";
 import Section from "./Section";
+import Tabs from "./Tabs";
 
-export default function Examples(){
+export default function Examples() {
 
   const [selectedTopic, setSelectedTopic] = useState();
 
@@ -11,25 +12,31 @@ export default function Examples(){
     setSelectedTopic(selectedTopic);
   }
 
-    return(
-        <Section title="Examples" id="examples">
-        <menu>
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs buttons={
+        <>
           <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
           <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
           <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
           <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>States</TabButton>
-        </menu>
 
+        </>
+      }
+      >
         {selectedTopic ? <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>
-              {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-        </div> : <p>Please select a topic</p>}
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div> : <p>Please select a topic</p>}
 
-      </Section>
-    );
+      </Tabs>
+
+
+    </Section>
+  );
 }
