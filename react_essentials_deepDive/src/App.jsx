66d8a9e1cd24391d725aa03103fd29b00fee427f
alfarofernando <alfarofernando@live.com>
2024-09-11 02:42:34 -0300

    rendering multidimensional list

--- react_essentials_deepDive/src/App.jsx
+++ react_essentials_deepDive/src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
             <Player defaultName="Player 2" symbol="O" />
         </ol>
 
-        GAMEBOARD
+        <GameBoard/>
 
       </div>
     </main>
