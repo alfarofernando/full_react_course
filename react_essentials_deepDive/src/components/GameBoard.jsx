966eb324d606de4f0b5335a0e2a8f1b1a770e4aa
alfarofernando <alfarofernando@live.com>
2024-09-13 02:58:13 -0300

    disabling gameboard buttons if were pressed

--- react_essentials_deepDive/src/components/GameBoard.jsx
+++ react_essentials_deepDive/src/components/GameBoard.jsx
@@ -46,7 +46,9 @@ export default function GameBoard({ onSelectSquare , turns }) {
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
                                 <button onClick={() =>
-                                    onSelectSquare(rowIndex, colIndex)}>
+                                    onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
                                     {playerSymbol}
                                 </button>
                             </li>
