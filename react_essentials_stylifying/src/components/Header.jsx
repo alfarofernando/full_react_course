a9ec95d4fc6d920b346e3452efc13573ff3550c8
alfarofernando <alfarofernando@live.com>
2024-09-29 07:09:13 -0300

    tailwind mediaquery n pseudoselectors

--- react_essentials_stylifying/src/components/Header.jsx
+++ react_essentials_stylifying/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import logo from "../assets/logo.png";
 
+const headerStyle = "flex flex-col items-center mt-8 mb-16 ";
+const headerImgStyle = "mb-8 w-44 h-44 object-contain ";
+const headerH1Style = "text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title ";
+const headerPStyle = "text-stone-500"; 
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16 " >
-      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain " />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title ">ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className={headerStyle} >
+      <img src={logo} alt="A canvas" className={headerImgStyle} />
+      <h1 className={headerH1Style}>ReactArt</h1>
+      <p className={headerPStyle} >A community of artists and art-lovers.</p>
     </header>
   );
 }
