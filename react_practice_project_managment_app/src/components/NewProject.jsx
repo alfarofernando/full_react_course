28c7ccef5ad716773d2c368374ed1378bb5c8c07
alfarofernando <alfarofernando@live.com>
2024-10-05 02:05:56 -0300

    input n buttons styled

--- react_practice_project_managment_app/src/components/NewProject.jsx
+++ react_practice_project_managment_app/src/components/NewProject.jsx
@@ -1,10 +1,10 @@
 import Input from "./Input";
 
 export default function NewProject() {
-    return <div>
-        <menu>
-            <li><button>Cancel</button></li>
-            <li><button>Save</button></li>
+    return <div className="w-[35rem] mt-16 ">
+        <menu className="flex items-center justify-center gap-4 my-4">
+            <li><button className="bg-red-200 text-red-700 hover:text-red-900 hover:bg-red-400 rounded-md p-2 ">Cancel</button></li>
+            <li><button className="bg-green-200 text-green-700 hover:text-green-900 hover:bg-green-400 rounded-md p-2 ">Save</button></li>
         </menu>
         <div>
             <Input label="Title"  />
