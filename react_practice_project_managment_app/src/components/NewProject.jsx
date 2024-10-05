fab0f0f51adb37c279ba230059222bb9a0e63896
alfarofernando <alfarofernando@live.com>
2024-10-05 04:28:46 -0300

    validating user inputs n showing a modal using imperativehandle

--- react_practice_project_managment_app/src/components/NewProject.jsx
+++ react_practice_project_managment_app/src/components/NewProject.jsx
@@ -1,36 +1,52 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-export default function NewProject({onAddProject}) {
+export default function NewProject({ onAddProject }) {
 
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
 
-function handleSave(){
-    const enteredTitle = title.current.value;
-    const enteredDescription = description.current.value;
-    const enteredDueDate = dueDate.current.value;
-    
-    // validation...
-    
-    onAddProject({
-        title: enteredTitle,
-        description: enteredDescription,
-        dueDate: enteredDueDate
-    });
-}
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
 
-return <div className="w-[35rem] mt-16 ">
-        <menu className="flex items-center justify-center gap-4 my-4">
-            <li><button className="bg-red-200 text-red-700 hover:text-red-900 hover:bg-red-400 rounded-md p-2 ">Cancel</button></li>
-            <li><button onClick={handleSave} className="bg-green-200 text-green-700 hover:text-green-900 hover:bg-green-400 rounded-md p-2 ">Save</button></li>
-        </menu>
-        <div>
-            <Input type="text" ref={title} label="Title"  />
-            <Input ref={description} isTextarea={true} label="description"  />
-            <Input type="date" ref={dueDate} label="due date"  />
-            
+        // validation...
+        if (enteredTitle.trim() === '' ||
+            enteredDescription.trim() === '' ||
+            enteredDueDate.trim() === ''
+        ) {
+            modal.current.open();
+            return;    
+        }
+
+        onAddProject({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        });
+    }
+
+    return <>
+        <Modal ref={modal} buttonCaption="Close">
+            <h2>Invalid Input</h2>
+            <p>Oops looks like you forgot to enter a value</p>
+            <p>Please make sure you provide a valid value for every input</p>
+        </Modal>
+        <div className="w-[35rem] mt-16 ">
+            <menu className="flex items-center justify-center gap-4 my-4">
+                <li><button className="bg-red-200 text-red-700 hover:text-red-900 hover:bg-red-400 rounded-md p-2 ">Cancel</button></li>
+                <li><button onClick={handleSave} className="bg-green-200 text-green-700 hover:text-green-900 hover:bg-green-400 rounded-md p-2 ">Save</button></li>
+            </menu>
+            <div>
+                <Input type="text" ref={title} label="Title" />
+                <Input ref={description} isTextarea={true} label="description" />
+                <Input type="date" ref={dueDate} label="due date" />
+
+            </div>
         </div>
-    </div>
+    </>
 }
