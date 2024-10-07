7c3fbad150ea7a613dfb3137e07c5832f470d66c
alfarofernando <alfarofernando@live.com>
2024-10-07 05:48:50 -0300

    linking context to state

--- react_advanced_state_management/src/App.jsx
+++ react_advanced_state_management/src/App.jsx
@@ -66,8 +66,13 @@ function App() {
     });
   }
 
+  const contextValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart
+  }
+
   return (
-    <CartContext.Provider value={{items:[]}}>
+    <CartContext.Provider value={contextValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -75,7 +80,7 @@ function App() {
       <Shop>
       {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
