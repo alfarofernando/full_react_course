a9b1effab613cb49053a34f1015f0f34fbb362fd
alfarofernando <alfarofernando@live.com>
2024-10-09 06:55:01 -0300

    understanding useeffect dependencies

--- react_advanced_effects/src/App.jsx
+++ react_advanced_effects/src/App.jsx
@@ -11,8 +11,8 @@ const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
 const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place)=> place.id === id));
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [ modalIsOpen , setModalIsOpen ] = useState(false);
   const [availablePlaces , setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -25,12 +25,12 @@ useEffect(()=>{
 }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -52,7 +52,7 @@ useEffect(()=>{
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter(
@@ -62,7 +62,7 @@ useEffect(()=>{
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
