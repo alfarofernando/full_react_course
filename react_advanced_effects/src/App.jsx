7c2641c088f1a3ca3900a7c83f14a3395fc2d15a
alfarofernando <alfarofernando@live.com>
2024-10-09 06:09:08 -0300

    when not to use useEffect

--- react_advanced_effects/src/App.jsx
+++ react_advanced_effects/src/App.jsx
@@ -7,11 +7,14 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import {sortPlacesByDistance} from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place)=> place.id === id));
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces , setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
 useEffect(()=>{
   navigator.geolocation.getCurrentPosition((position)=>{
@@ -20,7 +23,6 @@ useEffect(()=>{
   });
   
 }, []);
-
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -39,6 +41,11 @@ useEffect(()=>{
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if(storedIds.indexOf(id) === -1){
+      localStorage.setItem('selectedPlaces' , JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -46,6 +53,11 @@ useEffect(()=>{
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter(
+      (id) => id !== selectedPlace.current
+    )))
   }
 
   return (
@@ -75,7 +87,7 @@ useEffect(()=>{
         <Places
           title="Available Places"
           fallbackText="Sorting places by distance..."
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
           onSelectPlace={handleSelectPlace}
         />
       </main>
